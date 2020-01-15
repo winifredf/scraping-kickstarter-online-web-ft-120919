a27a725f3ce9ca0fd2cc8f5564963c0c5517a2f3
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 11:46:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,4 +10,13 @@ def create_project_hash
   
     projects = {}
   
+  end
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+ 
+  # return the projects hash
+  projects
 end
