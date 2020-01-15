7999edff88d0a38e77a19883937e64d56ee4223f
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 11:44:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,5 +9,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   
     projects = {}
-
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
 end
